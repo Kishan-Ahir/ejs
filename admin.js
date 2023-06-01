131e4c53f2c87bd841afe18576f85f3d500546a2
Kishan Chandravadiya <123851397+Kishan-Ahir@users.noreply.github.com>
2023-06-01 23:14:15 +0530

    Add files via upload

--- admin.js
+++ admin.js
@@ -1,24 +1,68 @@
-const path = require('path');
+const Product = require('../models/product');
 
-const express = require('express');
+exports.getAddProduct = (req, res, next) => {
+  res.render('admin/edit-product', {
+    pageTitle: 'Add Product',
+    path: '/admin/add-product',
+    editing:false
+  });
+};
 
-const adminController = require('../controllers/admin');
+exports.postAddProduct = (req, res, next) => {
+  const title = req.body.title;
+  const imageUrl = req.body.imageUrl;
+  const price = req.body.price;
+  const description = req.body.description;
+  const product = new Product(null, title, imageUrl, description, price);
+  product
+  .save()
+  .then(()=> res.redirect('/'))
+  .catch((err)=>{console.log(err)})  
+};
 
-const router = express.Router();
+exports.postEditProduct = (req,res,next) => {
+  const prodid = req.body.productId;
+  const updatedTitle = req.body.title;
+  const updatedPrice = req.body.price;
+  const updatedImageUrl = req.body.imgUrl;
+  const updatedDesc = req.body.description;
+  const updatedProduct = new Product(
+    prodid,
+    updatedTitle,
+    updatedImageUrl,
+    updatedDesc,
+    updatedPrice
+  );
+  updatedProduct.save();
+  res.redirect('/admin/products');
+};
 
-// /admin/add-product => GET
-router.get('/add-product', adminController.getAddProduct);
+exports.getEditProduct = (req, res, next) => {
+  const editMode = req.query.edit;
+  if(!editMode)
+  {
+    return res.redirect('/');
+  }
+  let prodid = req.params.productId;
+  Product.findbyid(prodid,product=>{
+    if (!product) {
+      return res.redirect("/");
+    }
+    res.render("admin/edit-product", {
+      pageTitle: "Edit Product",
+      path: "/admin/edit-product",
+      editing: editMode,
+      product: product
+    });
+  });
+};
 
-// /admin/products => GET
-router.get('/products', adminController.getProducts);
-
-// /admin/add-product => POST
-router.post('/add-product', adminController.postAddProduct);
-
-router.post("/edit-product", adminController.postEditProduct);
-
-router.get('/edit-product/:productId', adminController.getEditProduct);
-
-
-
-module.exports = router;
+exports.getProducts = (req, res, next) => {
+  Product.fetchAll(products => {
+    res.render('admin/products', {
+      prods: products,
+      pageTitle: 'Admin Products',
+      path: '/admin/products'
+    });
+  });
+};
--- shop.js
+++ shop.js
@@ -2,34 +2,41 @@ const Product = require('../models/product');
 const cart = require("../models/cart");
 
 exports.getProducts = (req, res, next) => {
-  Product.fetchAll(products => {
-    res.render('shop/product-list', {
-      prods: products,
-      pageTitle: 'All Products',
-      path: '/products'
-    });
-  });
+  Product.fetchAll()
+  .then(([rows,fielddata])=>{
+       res.render("shop/product-list", {
+         prods: rows,
+         pageTitle: "All Products",
+         path: "/products",
+       });
+  })
+  .catch(err => console.log(err))
+ 
 };
 
 exports.getproduct = (req,res,next) => {  
   let prodid = req.params.productId;
-  Product.findbyid(prodid,(product)=>{
-    res.render("shop/product-detail", {
-      product: product,
-      pageTitle: product.title,
-      path: '/products'
-    });
+  Product.findbyid(prodid)
+  .then(([product])=>{
+      res.render("shop/product-detail", {
+        product: product[0],
+        pageTitle: product.title,
+        path: "/products",
+      });
   })
 }
 
 exports.getIndex = (req, res, next) => {
-  Product.fetchAll(products => {
-    res.render('shop/index', {
-      prods: products,
-      pageTitle: 'Shop',
-      path: '/'
+  Product.fetchAll()
+  .then(([rows,fielddata]) => {
+    res.render("shop/index", {
+      prods: rows,
+      pageTitle: "Shop",
+      path: "/",
     });
-  });
+    console.log(fielddata);
+  })
+  .catch(err=>console.log(err));
 };
 
 exports.getCart = (req, res, next) => {
